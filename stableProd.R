4c365a18cf9e1836f724004b4e4214b73c76f33f
JB Duck-Mayr <j.duckmayr@gmail.com>
2017-03-14 15:45:21 -0500

    add a function to calculate numerically stable products

---
+++ stableProd.R
@@ -0,0 +1,14 @@
+#' Numerically stable product
+#' 
+#' \code{stableProd} calculates the product of a numeric vector
+#' 
+#' While base R provides the function \code{prod}, a more numerically stable
+#' way to calculate the product of a vector is to take the exponential of the
+#' sum of the natural logarithm of each element of the vector.
+#' 
+#' @x A numeric vector
+#' 
+#' @return The product of the elements of x.
+stableProd <- function(x){
+  return(exp(sum(log(x))))
+}
