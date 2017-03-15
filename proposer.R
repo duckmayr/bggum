ae28aa8e6c34933992e49dc77b39b40ce23817b9
JB Duck-Mayr <j.duckmayr@gmail.com>
2017-03-14 19:22:51 -0500

    alter decider and proposer in light of code for iteration

--- proposer.R
+++ proposer.R
@@ -5,14 +5,13 @@
 #' @param currentValue The current value of the parameter of interest
 #' @param paramType A character vector indicating the parameter type; should be
 #'   one of 'alpha', 'delta', 'tau', or 'theta'
-#' @param param In an unlisted vector of one item's alpha, delta, and tau
-#'   parameters, the index of the parameter of interest in this iteration
+#' @param item The item for the parameter of interest
 #' @param paramVector The vector or list of values for parameter paramType
 #' 
 #' @return A ratio used in the MCMC algorithm for the GGUM.
-proposer <- function(currentValue, paramType, param, paramVector){
+proposer <- function(currentValue, paramType, item, paramVector){
   if (paramType == 'tau') {
-    paramVector <- sapply(paramVector, safeExtract, param-2)
+    paramVector <- paramVector[[item]]
   }
   return(rnorm(1, currentValue, sd(paramVector)))
 }
