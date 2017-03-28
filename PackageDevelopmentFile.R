172807191b5964b7b050e9377f59eda3dbd44688
JB Duck-Mayr <j.duckmayr@gmail.com>
2017-03-28 14:57:54 -0500

    add package file structure and development file

---
+++ PackageDevelopmentFile.R
@@ -0,0 +1,9 @@
+## Package Development File Template ##
+library(devtools)
+library(roxygen2)
+current.code <- as.package('ggumR')
+load_all(current.code)
+document(current.code)
+# check(current.code)
+# install(pkg=current.code, local=TRUE)
+# build(current.code, path=getwd())
