058a112bafd23d6ecccc3e5bd6fa89fe09d50d5e
pcunhasilva <patricksilva27@gmail.com>
2017-03-29 01:53:31 -0500

    Add test_probabilityRange
    Add tests to probability’s range.

---
+++ ggumR/tests/testthat/test_probabilityRange.R
@@ -0,0 +1,31 @@
+context("Test if probability produces resutls in a range of values that is 
+        similar to the one observed in de la Torres 2006")
+# Define parameter to use in the test.
+alpha <- 2
+theta <- 0
+tau <- c(0, -1, -0.7, -0.4)
+K <- 4
+
+# For k = 4 and theta = 0 
+test_that("Output is less than 0.65",
+          expect_lte(probability(k = 4, K = K, theta = 0, alpha = alpha, delta = delta, tau = tau),  0.65))
+test_that("Output is greater than 0.6",
+          expect_gte(probability(k = 4, K = K, theta = 0, alpha = alpha, delta = delta, tau = tau),  0.6))
+
+# For k = 3 and theta = 0.25
+test_that("Output is less than 0.35",
+          expect_lte(probability(k = 3, K = K, theta = 0.25, alpha = alpha, delta = delta, tau = tau),  0.35))
+test_that("Output is greater than 0.3",
+          expect_gte(probability(k = 3, K = K, theta = 0.25, alpha = alpha, delta = delta, tau = tau),  0.3))
+
+# For k = 2 and theta = 1
+test_that("Output is less than 0.40",
+          expect_lte(probability(k = 2, K = K, theta = 1, alpha = alpha, delta = delta, tau = tau),  0.40))
+test_that("Output is greater than 0.35",
+          expect_gte(probability(k = 2, K = K, theta = 1, alpha = alpha, delta = delta, tau = tau),  0.35))
+
+# For k = 1 and theta = -3
+test_that("Output is less than 1",
+          expect_lte(probability(k = 1, K = K, theta = -3, alpha = alpha, delta = delta, tau = tau),  1))
+test_that("Output is greater than 0.96",
+          expect_gte(probability(k = 1, K = K, theta = -3, alpha = alpha, delta = delta, tau = tau),  0.96))
