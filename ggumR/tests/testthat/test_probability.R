3358555f7b57f1565cf9cce0baa264815f79edc7
JB Duck-Mayr <j.duckmayr@gmail.com>
2017-04-03 06:29:50 -0500

    increase precision of expected values in test_probability.R

--- ggumR/tests/testthat/test_probability.R
+++ ggumR/tests/testthat/test_probability.R
@@ -2,7 +2,7 @@ context("probability")
 
 test_that("Get the correct probability", {
   expect_that(probability(1, 2, 3, 1, 2, 3),
-              equals(0.6759729))
+              equals(0.675972863168057))
   expect_that(probability(1, 4, 1, 1, 1, c(-2, -1, 1, 2)),
-              equals(0.2060319))
+              equals(0.206031909190019))
 })
