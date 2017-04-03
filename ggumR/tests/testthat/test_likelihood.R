177e66873e5158b57740e3ac37822235c3c90f50
pcunhasilva <patricksilva27@gmail.com>
2017-04-02 21:44:00 -0500

    Add likelihood test
    Add test for likelihood function.

---
+++ ggumR/tests/testthat/test_likelihood.R
@@ -0,0 +1,16 @@
+context("Test if likelihood is always between 0 and 1")
+# Define the parameters to use in the test:
+set.seed(123)
+response_matrix <- cbind(sample(c(0:4), 1, replace = TRUE), 
+                         sample(c(0:4), 1, replace = TRUE), 
+                         sample(c(0:4), 1, replace = TRUE)) 
+alphas <- runif(3, 0, 2)
+deltas <- runif(3, 0, 1)
+thetas <- rnorm(1)
+taus <- list(c(0, runif(4, 0, 1)), c(0, runif(4, 0, 1)), c(0, runif(4, 0, 1))) # There in problem here.
+
+# Test likelihood \in [0,1]
+test_that("Output is equal or larger than 0", 
+          expect_gte(likelihood(thetas, response_matrix, alphas, deltas, taus), 0))
+test_that("Output is equal or less than 1",
+          expect_lte(likelihood(thetas, response_matrix, alphas, deltas, taus), 1))
