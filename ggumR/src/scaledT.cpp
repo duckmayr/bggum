ef90c9b071de75b3b7a0e2544f18f6ee9d565e91
JB Duck-Mayr <j.duckmayr@gmail.com>
2017-04-06 11:58:38 -0500

    add location-scale t distribution functions

---
+++ ggumR/src/scaledT.cpp
@@ -0,0 +1,56 @@
+#include <Rcpp.h>
+
+using namespace Rcpp;
+
+//' The location-scale Student's T distribution.
+//'
+//' Density, distribution function, quantile function, and random number
+//' generation for the Student's T distribution shifted by location parameter
+//' 'mu' and scaled by 'sigma'.
+//'
+//' @param n A numeric vector of length one; the number of numbers to generate
+//' @param x,q A numeric vector of the quantiles of interest
+//' @param p A numeric vector of the probabilities of interest
+//' @param df A numeric vector of length one; the degrees of freedom of the
+//'   distribution
+//' @param sigma A numeric vector of length one; the scale parameter
+//' @param mu A numeric vector of length one; the shifting parameter
+//'
+//' @return 'dScaledT' gives the density, 'pScaledT' gives the distribution
+//'     function, 'qScaledT' gives the quantile function, and 'rScaledT'
+//'     generates random numbers from the distribution
+//'     
+//' @references Jackman, Simon. 2009. \emph{Bayesian Analysis for the Social
+//'     Sciences}. Wiley. p. 507.
+//'
+//' @name Location-Scale T Distribution
+//' @rdname scaledT-dist
+void scaledT_distribution();
+
+//' @rdname scaledT-dist
+//' @export
+//[[Rcpp::export]]
+NumericVector dScaledT(NumericVector x, double df, double mu, double sigma){
+    return (1 / sigma) * dt((x - mu)/sigma, df);
+}
+
+//' @rdname scaledT-dist
+//' @export
+//[[Rcpp::export]]
+NumericVector pScaledT(NumericVector q, double df, double mu, double sigma){
+    return pt((q - mu)/sigma, df);
+}
+
+//' @rdname scaledT-dist
+//' @export
+//[[Rcpp::export]]
+NumericVector qScaledT(NumericVector p, double df, double mu, double sigma){
+    return qt(p, df) * sigma + mu;
+}
+
+//' @rdname scaledT-dist
+//' @export
+//[[Rcpp::export]]
+NumericVector rScaledT(int n, double df, double mu, double sigma){
+    return rt(n, df) * sigma + mu;
+}
