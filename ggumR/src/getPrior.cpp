8acc72c9d46b701aca9eb6aaa572b966ab4b0481
JB Duck-Mayr <j.duckmayr@gmail.com>
2017-04-30 07:28:21 -0500

    edit getPrior distribution parameters and update documentation

--- ggumR/src/getPrior.cpp
+++ ggumR/src/getPrior.cpp
@@ -4,35 +4,35 @@ using namespace Rcpp;
 
 //' GGUM getPrior
 //' 
-//' Get the prior values for the parameters Theta, Alpha, Delta, and Taus
-//' for the GGUM.
+//' Get the prior probability of values for the GGUM parameters theta, alpha,
+//' delta, and tau.
 //' 
-//' @param cv A numeric vector of length one. The current value of the parameter of interest
-//' @param tau The vector of current values of the items' tau parameters
+//' @param cv A numeric vector of length one; the current value of the
+//'   parameter of interest
 //' 
-//' @return A numeric vector of length one. The propose value for the paramenter of interest.
+//' @return A numeric vector of length one. The prior probability of observing
+//'   \code{cv} for the paramenter of interest.
 //' @rdname getPrior
 //' @export
 //[[Rcpp::export]]
 double getPriorTheta(double cv){
-  double cvPrior = d_4beta(cv, 2, 2, -5, 5);
+    return R::dnorm(cv, 0, 1, 0);
 }
 
 //' @export
 //[[Rcpp::export]]
 double getPriorAlpha(double cv){
-  double cvPrior = d_4beta(cv, 2, 2, -5, 5);
+    return d_4beta(cv, 1.5, 1.5, 0.25, 4);
 }
 
 //' @export
 //[[Rcpp::export]]
 double getPriorDelta(double cv){
-  double cvPrior = d_4beta(cv, 2, 2, -5, 5);
+    return d_4beta(cv, 2, 2, -5, 5);
 }
 
 //' @export
 //[[Rcpp::export]]
-double getPriorTaus(double cv, NumericVector taus){
-  double cvPrior = d_4beta(cv, 2, 2, -5, 5);
-  double cvPrior = d_4beta(taus[k], 2, 2, -6, 6);
+double getPriorTaus(double cv){
+    return d_4beta(cv, 2, 2, -6, 6);
 }
